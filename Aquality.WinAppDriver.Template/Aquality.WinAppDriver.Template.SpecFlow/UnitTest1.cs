4874e73304b579d0d8f79856e5cf9036efb21d45
Pavel Anihimovsky <p.anihimovsky@a1qa.com>
2020-04-16 12:18:35 +0300

    Create skeleton solution

---
+++ Aquality.WinAppDriver.Template/Aquality.WinAppDriver.Template.SpecFlow/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Aquality.WinAppDriver.Template.SpecFlow
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Aquality.WinAppDriver.Template/Aquality.WinAppDriver.Template/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Aquality.WinAppDriver.Template
+{
+    public class Class1
+    {
+    }
+}
